64a8672ba3ad9ac3eacd25301ff4271ecc3e91e0
mklakhan <mklakhan@gmail.com>
2021-03-19 16:32:56 -0700

    updates to files

---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config()
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.static('public'))
+
+app.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'))
+})
+
+app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname + '/public/stats.html'))
+})
+
+app.post('/api/workouts', (req,res) => {
+    console.log(request.body)
+    res.send('i got the data')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on ${process.env.PORT}`)
+})
